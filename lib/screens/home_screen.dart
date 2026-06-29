b4ddb9281ea45e1c534296d35bb8d6a054c51e1e
Z4hra25 <z4hra1426@gmail.com>
2026-06-29 21:01:53 +0300

    Create home screen UI

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Arabic Sign Translator"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("📷 Translate Sign"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("⌨️ Text to Sign"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("ℹ About"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
